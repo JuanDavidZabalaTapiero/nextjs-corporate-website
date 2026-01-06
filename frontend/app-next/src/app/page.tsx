a680f7dc2135ed297146dfabefddf6baef38d0d4
JuanDavidZabalaTapiero <juandavidzabalatapiero@gmail.com>
2026-01-05 20:33:16 -0500

    feat: sanity

--- frontend/app-next/src/app/page.tsx
+++ frontend/app-next/src/app/page.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
+import Image from 'next/image'
+
 import { sanityClient } from '@/src/lib/sanity'
+import { urlFor } from '@/src/lib/sanityImage'
 
 export const metadata: Metadata = {
   title: "Inicio",
@@ -7,12 +10,32 @@ export const metadata: Metadata = {
 
 const query = `
   *[_type == "home"][0]{
-    title
+    title,
+    subtitle,
+    heroImage{
+      asset,
+      alt
+    }
   }
 `
 
 export default async function HomePage() {
   const data = await sanityClient.fetch(query)
 
-  return <h1>{data?.title}</h1>
+  return (
+    <>
+      <h1 className="text-center font-bold">{data?.title}</h1>
+      <h2 className="text-center">{data?.subtitle}</h2>
+      {data.heroImage && (
+        <Image
+          src={urlFor(data.heroImage).width(1200).url()}
+          alt={data.heroImage.alt ?? 'Imagen principal'}
+          width={800}
+          height={600}
+          className="mx-auto"
+          priority
+        />
+      )}
+    </>
+  )
 }
--- frontend/studio-practice-web/schemaTypes/home.ts
+++ frontend/studio-practice-web/schemaTypes/home.ts
@@ -1,15 +1,37 @@
 import { defineType, defineField } from 'sanity'
 
 export default defineType({
-    name: 'home',
-    title: 'Inicio',
-    type: 'document',
-    fields: [
+    name: 'home', // ID (Uso en query)
+    title: 'Inicio', // Título en PANEL
+    type: 'document', // Indicar que es una página (Home, Contact, About)
+    fields: [ // Campos (títulos, párrafos, imgs)
         defineField({
-            name: 'title',
-            title: 'Título principal',
+            name: 'title', // Uso en query
+            title: 'Título principal', // Título en PANEL
             type: 'string',
             validation: Rule => Rule.required()
+        }),
+
+        defineField({
+            name: 'subtitle',
+            title: 'Subtítulo',
+            type: 'string'
+        }),
+
+        defineField({
+            name: 'heroImage',
+            title: 'Imagen principal',
+            type: 'image',
+            options: {
+                hotspot: true
+            },
+            fields: [
+                {
+                    name: 'alt',
+                    title: 'Texto alternativo',
+                    type: 'string'
+                }
+            ]
         })
     ]
 })
