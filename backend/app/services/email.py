f0e3009f7d3a75274783f8abe64981c1549b9953
JuanDavidZabalaTapiero <juandavidzabalatapiero@gmail.com>
2026-01-07 19:42:01 -0500

    update: SMTP_PORT in email.py

--- backend/app/services/email.py
+++ backend/app/services/email.py
@@ -3,7 +3,7 @@ import smtplib
 from email.message import EmailMessage
 
 SMTP_SERVER = os.getenv("SMTP_SERVER")
-SMTP_PORT = int(os.getenv("SMTP_PORT"))
+SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
 SMTP_USER = os.getenv("SMTP_USER")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 
